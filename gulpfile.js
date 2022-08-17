6b88865bb28ec3135c2b661066751a34ca7f0086
Andrey <fomenkoandrey1978@gmail.com>
2022-08-17 10:21:19 +0300

    create-structure

--- gulpfile.js
+++ gulpfile.js
@@ -17,8 +17,11 @@ const PATH = {
   cssFolder: './assets/css/',
   cssFiles: './assets/css/*.css',
   cssFile: './assets/css/style.css',
+  htmlFolder: './',
   htmlFiles: './*.html',
-  jsFiles: './assets/js/**/*.js'
+  jsFolder: './assets/js/',
+  jsFiles: './assets/js/**/*.js',
+  imgFolder: './assets/img/'
 };
 
 const PLUGINS = [
@@ -75,7 +78,44 @@ function watchFiles() {
   watch(PATH.jsFiles, sync);
 }
 
+function createStructure() {
+  let file = [];
+  let scssFiles = [];
+
+  scssFiles[0] = `${PATH.scssFolder}style.scss`;
+  scssFiles[1] = `${PATH.scssFolder}_variables.scss`;
+  scssFiles[2] = `${PATH.scssFolder}_skin.scss`;
+  scssFiles[3] = `${PATH.scssFolder}_common.scss`;
+  scssFiles[4] = `${PATH.scssFolder}_footer.scss`;
+  scssFiles[5] = `${PATH.scssFolder}_header.scss`;
+
+  file[0] = `${PATH.htmlFolder}index.html`;
+  file[1] = `${PATH.cssFolder}style.css`;
+  file[2] = `${PATH.jsFolder}main.js`;
+  file[3] = scssFiles;
+
+  src('*.*', {read: false})
+    .pipe(dest(PATH.scssFolder))
+    .pipe(dest(PATH.cssFolder))
+    .pipe(dest(PATH.jsFolder))
+    .pipe(dest(PATH.imgFolder));
+
+  return new Promise((resolve) => setTimeout(() => {
+    for (let i = 0; i < file.length; i++) if (!Array.isArray(file[i])) {
+      require('fs').writeFileSync(file[i], '');
+      console.log(file[i]);
+    } else for (let j = 0; j < file[i].length; j++) {
+      require('fs').writeFileSync(file[i][j], '');
+      console.log(file[i][j]);
+    }
+
+
+    resolve(true);
+  }, 1000));
+}
+
 task('comb', series(comb));
 task('scss', series(scss));
 task('dev', series(scssDev));
+task('cs', series(createStructure));
 task('watch', watchFiles);
